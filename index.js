07ad1845e11b318ede2eb4c85fc34f10d1c8a4c4
rs0579 <raheemsenegal@outlook.com>
2025-01-18 00:44:40 +0100

    code produces sought behaviour.

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ import inquirer from 'inquirer'
 import fs from 'fs'
 
 const generateREADME = (response) => {
-    `# <${response.name}>
+   return `# <${response.project}>
 
 ## Description
-${response.desription}
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 
 - ${response.motivation}
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+- ${response.build}
+- ${response.solve}
+- ${response.learn}
 
 ## Table of Contents (Optional)
 
@@ -24,33 +22,21 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${response.dependencies}
+${response.repo}
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
+${response.description}
+${response.screenshots}
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+${response.contribution}
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+${response.licence}
 
 ## Badges
 
@@ -64,60 +50,26 @@ If your project has a lot of features, list them here.
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${response.contribute}
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+${response.test}`
 }
 
 inquirer
     .prompt([
-        {
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "username"
-        },
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email"
 
-        },
         {
             type: "input",
             message: "What is your project's name?",
-            name: "project"
+            name: "project",
 
         },
         {
             type: "input",
             message: "Please write a description of your project.",
-            name: "description"
-
-        },
-        {
-            type: "input",
-            message: "What is your motivation?",
-            name: "motivation"
-
-        },
-        {
-            type: "input",
-            message: "why did you build the project?",
-            name: "build"
-
-        },
-        {
-            type: "input",
-            message: "What project does it solve?",
-            name: "solve"
-
-        },
-        {
-            type: "input",
-            message: "What's your makes your project stand out?",
-            name: "stand"
+            name: "description",
 
         },
         {
@@ -128,36 +80,77 @@ inquirer
 
         },
         {
+            type: "confirm",
+            message: "Are there any screenshots?",
+            name: "screenshots",
+        },
+        {
             type: "input",
             message: "What command should be run to install dependencies?",
-            name: "dependencies"
+            name: "dependencies",
 
         },
         {
             type: "input",
             message: "What should be run to run tests?",
-            name: "test"
+            name: "test",
 
         },
         {
             type: "input",
             message: "What does the user need to know about using the repo?",
-            name: "repo"
-
-        },
-        {
-            type: "input",
-            message: "What did you learn?",
-            name: "learn"
+            name: "repo",
 
         },
         {
             type: "input",
             message: "What does the user need to know about contributing to the repo?",
-            name: "contribution"
+            name: "contribution",
+
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username",
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+
+        },
+        {
+            type: "input",
+            message: "What's your makes your project stand out?",
+            name: "stand",
+
+        },
+        {
+            type: "input",
+            message: "What is your motivation?",
+            name: "motivation",
+
+        },
+        {
+            type: "input",
+            message: "why did you build the project?",
+            name: "build",
+
+        },
+        {
+            type: "input",
+            message: "What did you learn?",
+            name: "learn",
+
+        },
+        {
+            type: "input",
+            message: "What problem does it solve?",
+            name: "solve",
 
         },
 
-    ]).then((resposne) => 
-
-)
+    ]).then((response) => {
+        const mkREAD = generateREADME(response)
+        fs.writeFile('README.md', mkREAD, (err) => err ? console.log("There's something off here") : console.log("Success!"));
+    })
